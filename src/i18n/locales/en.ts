5131aa55da05d9148d1104d83aac71335a68525c
Ashwini Reddy <ashwinireddy@paraminnovation.org>
2025-03-06 06:52:56 +0530

    added

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -61,36 +61,12 @@ export const en = {
       "correctAnswer": 1
     },
     {
-      "text": "Which leader’s Rath Yatra played a crucial role in mobilizing people for the first Karseva?",
-      "options": [
-        "L. K. Advani",
-        "Atal Bihari Vajpayee"
-      ],
-      "correctAnswer": 0
-    },
-    {
-      "text": "Who was responsible for ordering police firing on Karsevaks in 1990?",
-      "options": [
-        "P V Narasimha Rao",
-        "Mulayam Singh Yadav"
-      ],
-      "correctAnswer": 1
-    },
-    {
       "text": "Which inscription provided strong historical evidence for the existence of the Ram Mandir?",
       "options": [
         "Ashokan Edicts",
         "Vishnu-Hari Inscription"
       ],
       "correctAnswer": 1
-    },
-    {
-      "text": "Which court ordered the excavation of the Ayodhya site in 2003?",
-      "options": [
-        "Allahabad High Court",
-        "Supreme Court of India"
-      ],
-      "correctAnswer": 0
     },
     {
       "text": "On which date did the Supreme Court deliver the final verdict on the Ram Janmabhoomi case?",
@@ -100,38 +76,6 @@ export const en = {
       ],
       "correctAnswer": 1
     },
-    {
-      "text": "Who was the senior lawyer who played a key role in arguing for the Hindu side in court?",
-      "options": [
-        "Ram Jethmalani",
-        "K. Parasaran"
-      ],
-      "correctAnswer": 1
-    },
-    {
-      "text": "Rama Mandir was built using:",
-      "options": [
-        "Voluntary donations from devotees",
-        "Funds provided by the government"
-      ],
-      "correctAnswer": 0
-    },
-    {
-      "text": "Which one of the following was discovered during the excavation of Ayodhya?",
-      "options": [
-        "Makar pranali - water discharging system",
-        "Minaret"
-      ],
-      "correctAnswer": 0
-    },
-    {
-      "text": "When did the first Karseva – a mass procession of devotees to Ayodhya – take place?",
-      "options": [
-        "1975",
-        "1990"
-      ],
-      "correctAnswer": 0
-    }
   ]
   
 };
